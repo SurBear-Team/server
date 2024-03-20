7e016a0f765c1be91df0a1ace5a0d3e814dac70d
jickDo <pocard0117@naver.com>
2024-03-20 18:13:00 +0900

    멀티모듈 설정

---
+++ surbear-api/src/main/java/com/surbear/SurbearApiApplication.java
@@ -0,0 +1,13 @@
+package com.surbear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurbearApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SurbearApiApplication.class, args);
+    }
+
+}
