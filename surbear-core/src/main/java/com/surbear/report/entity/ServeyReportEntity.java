3f3cc0abe2503c48666c53e00cdb2c1b4afb0201
jickDo <pocard0117@naver.com>
2024-04-04 16:00:16 +0900

    feature: 설문 신고 테이블 설계

---
+++ surbear-core/src/main/java/com/surbear/report/entity/ServeyReportEntity.java
@@ -0,0 +1,33 @@
+package com.surbear.report.entity;
+
+import com.surbear.common.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@EqualsAndHashCode(callSuper = false, onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "serveys_reports")
+public class ServeyReportEntity extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column
+    String reporterId;
+
+    @Column
+    String serveyId;
+
+    @Column
+    String reason;
+
+    @Column
+    Boolean deleted;
+}
