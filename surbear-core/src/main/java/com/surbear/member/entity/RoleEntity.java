768bb7a2620bf818c2c2a008cf5cb5fa78f23c87
jickDo <pocard0117@naver.com>
2024-04-04 14:11:03 +0900

    feature: 사용자 역할 엔티티 설계

---
+++ surbear-core/src/main/java/com/surbear/member/entity/RoleEntity.java
@@ -0,0 +1,35 @@
+package com.surbear.member.entity;
+
+import com.surbear.common.entity.BaseTimeEntity;
+import com.surbear.member.model.Role;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@EqualsAndHashCode(callSuper = false, onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "role")
+public class RoleEntity extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    @Column(name = "role_id")
+    Long id;
+
+    @Column
+    Long memberId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 50)
+    private Role role;
+
+    @Builder.Default
+    @Column
+    boolean deleted = false;
+}
