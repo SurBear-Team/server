af8ecbe8dcdee1567b46e2a517e9905856d2eba2
jickDo <pocard0117@naver.com>
2024-04-16 23:42:08 +0900

    feature: 설문 새 질문 만들기 api 와 로직

--- surbear-api/src/main/java/com/surbear/survey/question/controller/SurveyQuestionController.java
+++ surbear-api/src/main/java/com/surbear/survey/question/controller/SurveyQuestionController.java
@@ -1,4 +1,27 @@
 package com.surbear.survey.question.controller;
 
+
+import com.surbear.survey.question.dto.SurveyQuestionRequest;
+import com.surbear.survey.question.service.SurveyQuestionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Tag(name = "설문 질문생성", description = "설문진문 생성 관련 API")
+@RequiredArgsConstructor
+@RequestMapping("/survey")
 public class SurveyQuestionController {
+
+    private final SurveyQuestionService service;
+
+    @Operation(summary = "새 질문 만들기", description = "새 질문 만들기에 해당하는 API")
+    @PostMapping("/question")
+    public Long create(@RequestBody SurveyQuestionRequest req) {
+        return service.createQuestion(req.surveyQuestion(), req.answersRequest());
+    }
 }
--- surbear-api/src/main/java/com/surbear/survey/question/service/SurveyQuestionService.java
+++ surbear-api/src/main/java/com/surbear/survey/question/service/SurveyQuestionService.java
@@ -2,12 +2,15 @@ package com.surbear.survey.question.service;
 
 import com.surbear.mock.example.entity.example.ExampleEntity;
 import com.surbear.mock.example.model.Example;
+import com.surbear.survey.question.dto.AnswersRequest;
 import com.surbear.survey.question.entity.SurveyEntity;
+import com.surbear.survey.question.entity.SurveyQuestionEntity;
 import com.surbear.survey.question.entity.SurveyQuestionOptionEntity;
 import com.surbear.survey.question.mapper.SurveyMapper;
 import com.surbear.survey.question.mapper.SurveyQuestionMapper;
 import com.surbear.survey.question.mapper.SurveyQuestionOptionMapper;
 import com.surbear.survey.question.model.Survey;
+import com.surbear.survey.question.model.SurveyQuestion;
 import com.surbear.survey.question.model.SurveyQuestionOption;
 import com.surbear.survey.question.repository.SurveyQuestionOptionRepository;
 import com.surbear.survey.question.repository.SurveyQuestionRepository;
@@ -37,17 +40,21 @@ public class SurveyQuestionService {
         SurveyEntity surveyEntity = surveyMapper.toEntity(survey);
         return surveyRepository.save(surveyEntity).getId();
     }
-    // 작성자 id, 설문 제목, 설문 설명, 진행중 여부, 설문 유형,설문 비공개 여부,지급 포인트, 최대 인원
 
     @Transactional
-    public void createQuestion() {
-
+    public Long createQuestion(SurveyQuestion surveyQuestion,AnswersRequest req){
+        Long questionId = generateQuestion(surveyQuestion);
+        generateQuestionOption(req, questionId);
+        return questionId;
     }
-    // 질문 내용, 필수 여부, 질문 형식, 페이지, 설문지 테이블 id, 최대 글자수
 
-    @Transactional
-    public void createQuestionOption(List<String> answers, Long questionId) {
-        for (String answer : answers) {
+    private Long generateQuestion(SurveyQuestion surveyQuestion) {
+        SurveyQuestionEntity surveyQuestionEntity = surveyQuestionMapper.toEntity(surveyQuestion);
+        return surveyQuestionRepository.save(surveyQuestionEntity).getId();
+    }
+
+    private void generateQuestionOption(AnswersRequest req, Long questionId) {
+        for (String answer : req.answers()) {
             SurveyQuestionOption surveyQuestionOption = SurveyQuestionOption.builder()
                     .questionId(questionId)
                     .answer(answer)
