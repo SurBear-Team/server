76a6a5bf8e6052d59eb2ba93a22e06ac24ebea55
jickDo <pocard0117@naver.com>
2024-04-17 00:08:54 +0900

    hotfix: cors 대응

--- surbear-api/src/main/java/com/configuration/WebConfig.java
+++ surbear-api/src/main/java/com/configuration/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:8080")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST")
                 .allowCredentials(false)
                 .maxAge(3000);
