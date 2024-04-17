b9cc994c322fb95dfe49d384fe30a17f14da9f73
jickDo <pocard0117@naver.com>
2024-04-17 20:03:47 +0900

    chore: 설문 작성자 id와 deleted여부에 따른 조회 jpa

--- surbear-core/src/main/java/com/surbear/survey/question/repository/SurveyRepository.java
+++ surbear-core/src/main/java/com/surbear/survey/question/repository/SurveyRepository.java
@@ -1,7 +1,12 @@
 package com.surbear.survey.question.repository;
 
+import com.surbear.survey.dto.GetSurveyListResponse;
 import com.surbear.survey.question.entity.SurveyEntity;
+import com.surbear.survey.question.entity.SurveyQuestionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SurveyRepository extends JpaRepository<SurveyEntity, Long> {
+    List<GetSurveyListResponse> findBySurveyAuthorIdAndDeletedFalse(Long surveyAuthorId);
 }
