4a00111bc18c44e6b68c5c3d993e6f8216dd422e
jickDo <pocard0117@naver.com>
2024-04-17 20:03:05 +0900

    chroe : 마감일 추가

--- surbear-core/src/main/resources/db/migration/V20240407224619__surveys.sql
+++ surbear-core/src/main/resources/db/migration/V20240407224619__surveys.sql
@@ -12,5 +12,6 @@ CREATE TABLE IF NOT EXISTS surveys
     deleted                  BIT       DEFAULT 0                              NOT NULL COMMENT '삭제 여부',
     created_at               TIMESTAMP DEFAULT CURRENT_TIMESTAMP              NOT NULL COMMENT '생성 시간',
     updated_at               TIMESTAMP DEFAULT CURRENT_TIMESTAMP              NOT NULL COMMENT '수정 시간',
+    dead_line                TIMESTAMP DEFAULT CURRENT_TIMESTAMP              NOT NULL COMMENT '설문 종료 시간',
     PRIMARY KEY (id)
 );
