1c0fac7a1b7ba38ae662efe0d5d72850cf545818
jickDo <pocard0117@naver.com>
2024-04-18 20:01:29 +0900

    chore: dto 반환형 변경

--- surbear-core/src/main/java/com/surbear/survey/dto/SurveyQuestionRequest.java
+++ surbear-core/src/main/java/com/surbear/survey/dto/SurveyQuestionRequest.java
@@ -1,11 +1,14 @@
 package com.surbear.survey.dto;
 
 import com.surbear.survey.question.model.SurveyQuestion;
+import com.surbear.survey.question.model.SurveyQuestionOption;
 import lombok.Builder;
+
+import java.util.List;
 
 @Builder
 public record SurveyQuestionRequest(
-        AnswersRequest answersRequest,
+        List<SurveyQuestionOption> surveyQuestionOptions,
         SurveyQuestion surveyQuestion
 ) {
 }
