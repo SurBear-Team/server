feedb26e6ec79da8a867324ae80ba8351faedf12
jickDo <pocard0117@naver.com>
2024-04-18 20:01:15 +0900

    chore: jpa구문 추가

--- surbear-core/src/main/java/com/surbear/survey/question/repository/SurveyRepository.java
+++ surbear-core/src/main/java/com/surbear/survey/question/repository/SurveyRepository.java
@@ -1,12 +1,29 @@
 package com.surbear.survey.question.repository;
 
 import com.surbear.survey.dto.GetSurveyListResponse;
+import com.surbear.survey.dto.UpdateSurveyRequest;
 import com.surbear.survey.question.entity.SurveyEntity;
 import com.surbear.survey.question.entity.SurveyQuestionEntity;
+import com.surbear.survey.question.model.Survey;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface SurveyRepository extends JpaRepository<SurveyEntity, Long> {
-    List<GetSurveyListResponse> findBySurveyAuthorIdAndDeletedFalse(Long surveyAuthorId);
+
+    @Query("""
+            UPDATE SurveyEntity s
+            SET s.title = :#{#dto.title},
+                s.description = :#{#dto.description},
+                s.surveyType = :#{#dto.surveyType},
+                s.openType = :#{#dto.openType},
+                s.maximumNumberOfPeople = :#{#dto.maximumNumberOfPeople},
+                s.deadLine = :#{#dto.deadLine}
+            WHERE s.id = :id
+            """)
+    int updateSurvey(UpdateSurveyRequest dto, Long id);
+
+    List<Survey> findBySurveyAuthorId(Long surveyAuthorId);
+
 }
