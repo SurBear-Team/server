b4e7b7c076d3f3b2eacee38a8bd89a63eff45e75
jickDo <pocard0117@naver.com>
2024-04-18 22:38:56 +0900

    chore: jpa 구문추가

--- surbear-core/src/main/java/com/surbear/survey/question/repository/SurveyQuestionOptionRepository.java
+++ surbear-core/src/main/java/com/surbear/survey/question/repository/SurveyQuestionOptionRepository.java
@@ -2,6 +2,13 @@ package com.surbear.survey.question.repository;
 
 import com.surbear.survey.question.entity.SurveyQuestionOptionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface SurveyQuestionOptionRepository extends JpaRepository<SurveyQuestionOptionEntity,Long> {
+    @Transactional
+    @Modifying
+    @Query("UPDATE SurveyQuestionOptionEntity sqoe SET sqoe.deleted = true WHERE sqoe.questionId = :questionId AND sqoe.deleted = false")
+    void markDeletedByQuestionId(Long questionId);
 }
--- surbear-core/src/main/java/com/surbear/survey/question/repository/SurveyQuestionRepository.java
+++ surbear-core/src/main/java/com/surbear/survey/question/repository/SurveyQuestionRepository.java
@@ -2,6 +2,12 @@ package com.surbear.survey.question.repository;
 
 import com.surbear.survey.question.entity.SurveyQuestionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
 
 public interface SurveyQuestionRepository extends JpaRepository<SurveyQuestionEntity,Long> {
+    List<SurveyQuestionEntity> findAllBySurveyId(Long surveyId);
+    SurveyQuestionEntity findBySurveyId(Long surveyQuestionId);
+    SurveyQuestionEntity findFirstBySurveyIdAndDeletedIsFalse(Long surveyId);
+
 }
