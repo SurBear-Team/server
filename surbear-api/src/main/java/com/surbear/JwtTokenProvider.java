9694aafcc452c13fbc8b5510b088461048bc8776
jickDo <pocard0117@naver.com>
2024-04-23 19:11:43 +0900

    feature: jwt토큰생성 로직 구현

---
+++ surbear-api/src/main/java/com/surbear/JwtTokenProvider.java
@@ -0,0 +1,51 @@
+package com.surbear;
+
+import com.surbear.exception.ProcessErrorCodeException;
+import com.surbear.exception.errorcode.BasicErrorCode;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    private SecretKey key;
+    private long validityInMilliseconds;
+
+    public JwtTokenProvider(@Value("${security.jwt.token.secret-key}") String secretKey,
+                            @Value("${security.jwt.token.expire-length}") long validityInMilliseconds) {
+        this.key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+        this.validityInMilliseconds = validityInMilliseconds;
+    }
+
+
+    public String createToken(String payload) {
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + validityInMilliseconds);
+        return Jwts.builder()
+                .setSubject(payload)
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String getPayload(String token) throws ProcessErrorCodeException {
+        try {
+            return Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody()
+                    .getSubject();
+        } catch (ProcessErrorCodeException e) {
+            throw new ProcessErrorCodeException(BasicErrorCode.NOT_AUTHENTICATION);
+        }
+    }
+}
