4fb705023ed0d3bfb630d9e1673c60dc654c22fe
jickDo <pocard0117@naver.com>
2024-04-24 16:07:35 +0900

    feature: 설문타입 수정후 경우에 반환시 타입, 필수여부, 페이지 같이 반환하게 변경

--- surbear-core/src/main/java/com/surbear/survey/constants/QuestionType.java
+++ surbear-core/src/main/java/com/surbear/survey/constants/QuestionType.java
@@ -5,5 +5,5 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum QuestionType {
 
-    MULTIPLE_CHOICE, SHORT_ANSWER, SLIDER
-    }
+    SINGLE_CHOICE, MULTIPLE_CHOICE, SHORT_ANSWER, SUBJECTIVE, SLIDER
+}
--- surbear-core/src/main/java/com/surbear/survey/dto/QuestionAndOptions.java
+++ surbear-core/src/main/java/com/surbear/survey/dto/QuestionAndOptions.java
@@ -1,17 +1,19 @@
 package com.surbear.survey.dto;
 
+import com.surbear.survey.constants.QuestionType;
 import lombok.Builder;
 
 import java.util.List;
 
 @Builder
-public record QuestionAndOptions(Long questionId, String content, List<String> options) {
+public record QuestionAndOptions(Long questionId, QuestionType type, String content,
+                                 boolean required, Integer page, List<String>options) {
 
-    public static QuestionAndOptions ofSubjectiveQuestion(Long questionText, String content) {
-        return new QuestionAndOptions(questionText, content, List.of());
+    public static QuestionAndOptions ofSubjectiveQuestion(Long questionId, QuestionType type, String content, boolean required, Integer page) {
+        return new QuestionAndOptions(questionId, type, content, required, page, List.of());
     }
 
-    public static QuestionAndOptions ofObjectiveQuestion(Long questionText, String content, List<String> options) {
-        return new QuestionAndOptions(questionText, content, List.copyOf(options));
+    public static QuestionAndOptions ofObjectiveQuestion(Long questionId, QuestionType type, String content, boolean required, Integer page, List<String> options) {
+        return new QuestionAndOptions(questionId, type, content, required, page, List.copyOf(options));
     }
 }
