4429a7fdfcd91790b67235a68e688e1030ce462a
jickDo <pocard0117@naver.com>
2024-05-01 16:40:14 +0900

    feature: 프로필이라는 개별된 책임을 분리하기 위해 서비스와 컨트롤러를 분리후 설문조사 갯수와 참여 설문조사 갯수 api생성

---
+++ surbear-api/src/main/java/com/surbear/member/controller/MemberMyPageController.java
@@ -0,0 +1,41 @@
+package com.surbear.member.controller;
+
+
+import com.surbear.configuration.authorization.Authorization;
+import com.surbear.member.service.MemberProfileService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Tag(name = "회원 프로필", description = "프로필")
+@RequiredArgsConstructor
+@RequestMapping("/member/profile")
+public class MemberMyPageController {
+
+
+    private final MemberProfileService memberProfileService;
+
+
+    @Operation(summary = "등록한 설문조사 갯수", description = "토큰기반 등록한 설문조사 갯수 불러오기")
+    @GetMapping("/counting/survey")
+    public Integer getSurveyCount(
+            @Authorization
+            @Parameter(hidden = true)
+            Long memberId) {
+        return memberProfileService.getSurveyCount(memberId);
+    }
+
+    @Operation(summary = "참여한 설문조사 갯수", description = "토큰기반 참여한 설문조사 갯수 불러오기")
+    @GetMapping("/counting/participation")
+    public Integer getParticipationSurveyCount(
+            @Authorization
+            @Parameter(hidden = true)
+            Long memberId) {
+        return memberProfileService.getParticipationSurveyCount(memberId);
+    }
+}
