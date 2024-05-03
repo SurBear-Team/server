46eaa903131b3ebb7ab154dfb8da1d4dd4660e39
jickDo <pocard0117@naver.com>
2024-05-03 19:50:18 +0900

    hotfix: cors수정 테스트

--- surbear-api/src/main/java/com/surbear/configuration/WebConfig.java
+++ surbear-api/src/main/java/com/surbear/configuration/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000","http://localhost:8080","http://3.37.53.15:3000","http://3.37.53.15:8080")
+                .allowedOrigins("http://localhost:3000","http://localhost:8080","http://3.37.53.15:3000","http://3.37.53.15:8080","https://bizapi.giftishow.com")
                 .allowedHeaders("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
