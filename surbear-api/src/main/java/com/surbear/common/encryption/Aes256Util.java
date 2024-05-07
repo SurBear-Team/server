5c7e78dac13dbf16fb1ead60404167cf070af64e
jickDo <pocard0117@naver.com>
2024-05-07 21:45:48 +0900

    test: 암호화테스트

--- surbear-api/src/main/java/com/surbear/common/encryption/Aes256Util.java
+++ surbear-api/src/main/java/com/surbear/common/encryption/Aes256Util.java
@@ -17,11 +17,16 @@ public class Aes256Util {
     }
 
 
-    public String encrypt(String tokenKey, String authorizationKey) throws Exception {
-        SecretKeySpec secretKey = new SecretKeySpec(authorizationKey.getBytes(), "AES");
+    public String encrypt(String base64Data, String key) throws Exception {
+
+        byte[] dataBytes = Base64.getDecoder().decode(base64Data);
+        SecretKeySpec secretKey = new SecretKeySpec(key.getBytes(), "AES");
+
         Cipher cipher = Cipher.getInstance(ALGORITHM);
         cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-        byte[] encrypted = cipher.doFinal(tokenKey.getBytes());
+
+        byte[] encrypted = cipher.doFinal(dataBytes);
+
         return Base64.getEncoder().encodeToString(encrypted);
     }
 }
