2efd8b0d8a902fc5d9ff81f980b875e797b8e010
jickDo <pocard0117@naver.com>
2024-05-23 00:42:29 +0900

    chore: cors

--- surbear-api/src/main/java/com/surbear/configuration/WebConfig.java
+++ surbear-api/src/main/java/com/surbear/configuration/WebConfig.java
@@ -10,7 +10,13 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:8080", "http://3.37.53.15:3000", "http://3.37.53.15:8080","https://api.surbear.site:443")
+                .allowedOrigins("http://localhost:3000"
+                        , "http://localhost:8080"
+                        , "http://3.37.53.15:3000"
+                        , "http://3.37.53.15:8080"
+                        , "https://api.surbear.site:443"
+                        , "http://surbear.s3-website.ap-northeast-2.amazonaws.com"
+                        , "https://surbear.s3-website.ap-northeast-2.amazonaws.com")
                 .allowedHeaders("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
